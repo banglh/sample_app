0e9b22b300d7aba479e5d060c88d476099313166
bangle <ice.banglh@gmail.com>
2013-04-23 16:23:09 +0700

    Finish layout and routes

--- spec/features/static_pages_spec.rb
+++ spec/features/static_pages_spec.rb
@@ -1,6 +1,7 @@
 require 'spec_helper'
 
 describe "Static pages" do
+
   describe "Home page" do
     it "should have the h1 'Sample App'" do
       visit '/static_pages/home'
@@ -11,6 +12,7 @@ describe "Static pages" do
       page.should have_selector('title', :text => "Ruby on Rails Tutorial Sample App | Home")
     end
   end
+
   describe "Help page" do
     it "should have the h1 'Help'" do
       visit '/static_pages/help'
@@ -21,6 +23,7 @@ describe "Static pages" do
       page.should have_selector('title', :text => "Ruby on Rails Tutorial Sample App | Help")
     end
   end
+
   describe "About page" do
     it "should have the h1 'About Us'" do
       visit '/static_pages/about'
@@ -29,6 +32,17 @@ describe "Static pages" do
     it "should have the title 'About Us'" do
       visit '/static_pages/about'
       page.should have_selector('title', :text => "Ruby on Rails Tutorial Sample App | About Us")
+    end
+  end
+
+  describe "Contact page" do
+    it "should have the h1 'Contact'" do
+      visit '/static_pages/contact'
+      page.should have_selector('h1', text: 'Contact')
+    end
+    it "should have the title 'Contact'" do
+      visit '/static_pages/contact'
+      page.should have_selector('title', text: "Ruby on Rails Tutorial Sample App | Contact")
     end
   end
 end
